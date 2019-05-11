5c6955cd74ab714298edcfef7b4cabd4b4f37b05
copperly123 <aidancspendlove@gmail.com>
2019-05-10 19:31:18 -0600

    add projects and fix the rsync command modification

--- install.sh
+++ install.sh
@@ -14,25 +14,33 @@ then
 fi
 echo 'Installing Sinker'
 #give all executables executable permissions
-sudo chmod +x sinker aliases.sh uninstall.sh rsync_commands.sh
+sudo chmod a+x sinker aliases.sh uninstall.sh rsync_commands.sh projects.sh
 #copy executables to /usr/local/bin
 sudo cp sinker /usr/local/bin
-if [ ! -d "~/.config/Sinker/" ]
+#check if ~/.config/Sinker/ exists
+if [ ! -d ~/.config/Sinker/ ]
 then
 	#create it if it doesn't already exist
-mkdir ~/.config/Sinker/
+	mkdir ~/.config/Sinker/
 fi
+#copy all config files and unisntall.sh to ~/.config/Sinker/
 cp aliases.sh ~/.config/Sinker/
 cp rsync_commands.sh ~/.config/Sinker/
+cp projects.sh ~/.config/Sinker/
+cp uninstall.sh ~/.config/Sinker/
 echo '
 '
 #check if the restart flag is set
 if [ $restart != 1 ]
 then
 	#if not then tell the user that the program is installed
-	echo "fully installed"
+	echo "fully installed, you can delete this folder if you want"
+	echo "the uninstall script is located in ~/.config/Sinker/ and is named unisntall.sh"
+	echo "for more information go to https://github.com/copperly123/Sinker and reference the section on uninstalling"
 else
 	#if so then tell the user that they will need to restart
-	echo "you will need to reboot to fully install"
+	echo "you will need to reboot to fully install, after that you can delete this folder if you want"
+	echo "the uninstall script is located in ~/.config/Sinker/ and is named unisntall.sh"
+	echo "for more information go to https://github.com/copperly123/Sinker and reference the section on uninstalling"
 fi
 exit
